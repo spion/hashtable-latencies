57d00ca3bb25fd11f5f4333ea221c74053fe00ef
spion <gorgi.kosev@gmail.com>
2016-06-05 23:11:09 +0100

    ocaml: use Buffer instead of Array, avoids 8x overhead

--- ocaml-reason/Main.ml
+++ ocaml-reason/Main.ml
@@ -2,17 +2,17 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 open Hashtbl
-open Array
-open Char
+open Buffer
+
 let limit = 250000
-let tbl: (int,char array) Hashtbl.t = Hashtbl.create limit
+let tbl: (int,Buffer.t) Hashtbl.t = Hashtbl.create limit
 type counter = {
   mutable c: int;}
 let server =
   let cnt: counter = { c = 0 } in
   let callback _conn req body =
     let k = cnt.c in
-    Hashtbl.add tbl k (Array.make 1024 (Char.chr (k mod 256)));
+    Hashtbl.add tbl k (Buffer.create 1024);
     if k >= limit then Hashtbl.remove tbl (k - limit);
     cnt.c <- cnt.c + 1;
     Server.respond_string ~status:`OK ~body:"OK" () in

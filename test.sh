d5b8b4243df9d6a1b3e441787bdc76f0a693336c
Damjan Georgievski <gdamjan@gmail.com>
2016-06-05 23:40:59 +0200

    pushd/popd, remove unnecessary $cmd, report mem

--- test.sh
+++ test.sh
@@ -7,24 +7,24 @@ fi
 
 runtest() {
   local program="$1"
-  local cmd="$2"
   echo "Running $program..."
-  $cmd &
+  ./run.sh &
   local pid=$!
   sleep 3
   echo "Warming up $program..."
   wrk2 --latency -c 99 -t 3 -d 60 -R9000 'http://localhost:8080' | head -n17
   echo "Generating $program report..."
   wrk2 --latency -c 99 -t 3 -d 180 -R9000 'http://localhost:8080' > "../reports/$program"
+  grep -E '(VmRSS|VmPeak)' /proc/$pid/status
   kill -s TERM $pid
   sleep 3
 }
 
 for program in $programs
 do
-  cd $program
+  pushd $program
   echo "Building $program..."
   ./build.sh
-  runtest $program "./run.sh"
-  cd ..
+  runtest "$program"
+  popd
 done
